f2d8ef4224812054101e7f29ea393b69fe17189a
Kevin Tindall <kevinkjt2000@gmail.com>
2017-03-15 11:14:56 -0500

    fixed ellipsis.sh indentation

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,37 +3,37 @@
 # kevinkjt2000/files ellipsis package
 
 pkg.link() {
-  if [ $(hostname) = "Toxicity" ]; then
-    fs.link_file asoundrc
-  fi
-  fs.link_file gitconfig
-  mkdir -p "$ELLIPSIS_HOME/.xmonad"
-  fs.link_file xmonad/xmonad.hs "$ELLIPSIS_HOME/.xmonad/xmonad.hs"
-  mkdir -p "$ELLIPSIS_HOME/.config/openbox"
-  fs.link_file config/openbox/lxde-rc.xml "$ELLIPSIS_HOME/.config/openbox/lxde-rc.xml"
-  fs.link_file config/gitignore_global "$ELLIPSIS_HOME/.config/gitignore_global"
+	if [ $(hostname) = "Toxicity" ]; then
+		fs.link_file asoundrc
+	fi
+	fs.link_file gitconfig
+	mkdir -p "$ELLIPSIS_HOME/.xmonad"
+	fs.link_file xmonad/xmonad.hs "$ELLIPSIS_HOME/.xmonad/xmonad.hs"
+	mkdir -p "$ELLIPSIS_HOME/.config/openbox"
+	fs.link_file config/openbox/lxde-rc.xml "$ELLIPSIS_HOME/.config/openbox/lxde-rc.xml"
+	fs.link_file config/gitignore_global "$ELLIPSIS_HOME/.config/gitignore_global"
 	fs.link_file emacs
 	fs.link_file ctags
 }
 
 pkg.links() {
-  msg.bold "${1:-$PKG_NAME}"
-  local files=".gitconfig .xmonad/xmonad.hs .config/openbox/lxde-rc.xml .emacs"
+	msg.bold "${1:-$PKG_NAME}"
+	local files=".gitconfig .xmonad/xmonad.hs .config/openbox/lxde-rc.xml .emacs"
 	files+=" .config/gitignore_global .ctags"
-  if [ $(hostname) = "Toxicity" ]; then
-    files+=" .asoundrc"
-  fi
-  for file in $files; do
-    local file="$ELLIPSIS_HOME/$file"
-    local link="$(readlink "$file")"
-    echo "$(path.relative_to_packages "$link") -> $(path.relative_to_home "$file")";
-  done
+	if [ $(hostname) = "Toxicity" ]; then
+		files+=" .asoundrc"
+	fi
+	for file in $files; do
+		local file="$ELLIPSIS_HOME/$file"
+		local link="$(readlink "$file")"
+		echo "$(path.relative_to_packages "$link") -> $(path.relative_to_home "$file")";
+	done
 }
 
 pkg.unlink() {
-  rm "$ELLIPSIS_HOME/.config/openbox/lxde-rc.xml"
-  rm "$ELLIPSIS_HOME/.config/gitignore_global"
-  rm "$ELLIPSIS_HOME/.xmonad/xmonad.hs"
-  hooks.unlink
+	rm "$ELLIPSIS_HOME/.config/openbox/lxde-rc.xml"
+	rm "$ELLIPSIS_HOME/.config/gitignore_global"
+	rm "$ELLIPSIS_HOME/.xmonad/xmonad.hs"
+	hooks.unlink
 }
 
