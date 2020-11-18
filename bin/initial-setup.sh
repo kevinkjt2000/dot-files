3e27e4acfb604605295f923eedba35e35c42ce77
Kevin Tindall <kevinkjt2000@gmail.com>
2020-11-18 11:19:10 -0600

    Improve initial setup

--- bin/initial-setup.sh
+++ bin/initial-setup.sh
@@ -6,14 +6,15 @@ ln -sf "$(pwd)/asdf" "$HOME/.asdf"
 
 set +u
 source "$HOME/.asdf/asdf.sh"
-set -u
+set -ux
 
 source "$HOME/.asdf/completions/asdf.bash"
 if ! asdf plugin-list | grep python >/dev/null 2>&1; then
   asdf plugin-add python
 fi
-asdf install python 3.8.3
-python -m pip install --upgrade pip
+asdf install
+asdf reshim
+python -m pip install --upgrade pip wheel
 python -m pip install --upgrade dotdrop
 asdf reshim python
 dotdrop install
